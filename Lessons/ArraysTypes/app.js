b72b9d88ed64c3f38f7a0bc4a02f8726347de459
Marcin Maciorowski <m.maciorowski1991@gmail.com>
2023-01-01 13:56:23 +0100

    Working with tuple types

--- Lessons/ArraysTypes/app.js
+++ Lessons/ArraysTypes/app.js
@@ -5,7 +5,8 @@ var person = {
         street: 'Wrocławska',
         code: '55-555'
     },
-    hobbies: ['Programming', 'Sleeping']
+    hobbies: ['Programming', 'Sleeping'],
+    role: [2, 'author']
 };
 var favoriteActivities = []; // creating a variable with type array of strings
 // favoriteActivities = 'Sleeping' // impossible because this variable has type of array of strings
@@ -15,3 +16,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase()); // because TS knows what type of hobby is, it can tell us witch methods can be safely used on hobby!
 }
+person.role.push(1); // TS allows to push new elements to the tuple
+// person.role = ['ad', 'ad'] // this is not allowed because tuple element on index 0 should be type of number
+// person.role = [1, 'ad', 'e'] // this is not allowed to assign to a tuple array witch is not compatible with tuple type
+console.log(person.role);
--- Lessons/ArraysTypes/app.ts
+++ Lessons/ArraysTypes/app.ts
@@ -1,11 +1,22 @@
-const person = {
+const person: {
+    name: string
+    age: number
+    address: {
+        street: string
+        code: string
+    }
+    hobbies: string[]
+    role: [number, string] // this is a tuple!
+
+} = {
     name: 'Marcin',
     age: 31,
     address: {
         street: 'Wrocławska',
         code: '55-555'
     },
-    hobbies: ['Programming', 'Sleeping']
+    hobbies: ['Programming', 'Sleeping'],
+    role: [2, 'author']
 }
 
 
@@ -17,3 +28,9 @@ favoriteActivities.push('Sleeping') // possible because this pushes string to an
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase()) // because TS knows what type of hobby is, it can tell us witch methods can be safely used on hobby!
 }
+
+
+person.role.push(1) // TS allows to push new elements to the tuple
+// person.role = ['ad', 'ad'] // this is not allowed because tuple element on index 0 should be type of number
+// person.role = [1, 'ad', 'e'] // this is not allowed to assign to a tuple array witch is not compatible with tuple type
+console.log(person.role)
