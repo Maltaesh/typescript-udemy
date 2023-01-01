33becd3add9b744218d047a249d9ae4e97200a86
Marcin Maciorowski <m.maciorowski1991@gmail.com>
2023-01-01 18:04:05 +0100

    Working with unknown type

---
+++ Lessons/UnknownAndNeverTypes/app.js
@@ -0,0 +1,16 @@
+var userInput;
+var userName;
+userInput = 5;
+console.log(userInput);
+userInput = 'Marcin';
+// userName = userInput // not allowed to assign type string to type unknown. Needed to check type of userInput i.e.
+if (typeof userInput === 'string') {
+    userName = userInput; // this is allowed because of if-statement where userInput type is checked to be string
+    console.log('I am a string!');
+}
+var userInput2;
+var userName2;
+userInput2 = 5;
+console.log(userInput2);
+userInput2 = 'Marcin';
+userName2 = userInput; // allowed to assign type string to type any
---
+++ Lessons/UnknownAndNeverTypes/app.ts
@@ -0,0 +1,19 @@
+let userInput: unknown
+let userName: string
+userInput = 5
+console.log(userInput)
+userInput = 'Marcin'
+// userName = userInput // not allowed to assign type string to type unknown. Needed to check type of userInput i.e.
+if (typeof userInput === 'string') {
+    userName = userInput // this is allowed because of if-statement where userInput type is checked to be string
+    console.log('I am a string!')
+}
+
+
+let userInput2: unknown
+let userName2: any
+userInput2 = 5
+console.log(userInput2)
+userInput2 = 'Marcin'
+userName2 = userInput // allowed to assign type string to type any
+
