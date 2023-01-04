c102282fdcf1f0c02b171639818ae2bfef0000eb
Marcin Maciorowski <m.maciorowski1991@gmail.com>
2023-01-04 21:41:37 +0100

    Working with public and private modifiers

--- Lessons/ClassesAndInterfaces/creatingFirstClass/app.js
+++ Lessons/ClassesAndInterfaces/creatingFirstClass/app.js
@@ -1,14 +1,2 @@
 "use strict";
-class Department {
-    constructor(n) {
-        this.name = n;
-    }
-    getName() {
-        return this.name;
-    }
-}
-const accounting = new Department('Accounting');
-console.log(accounting);
-console.log(accounting.getName());
-const accountingCopy = { name: 'a', describe: accounting.getName };
 //# sourceMappingURL=app.js.map
