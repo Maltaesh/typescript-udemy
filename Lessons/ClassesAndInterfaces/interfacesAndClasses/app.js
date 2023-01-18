3a3ee37de81381f74c2d7cac581a0d4d605e8dfa
Marcin Maciorowski <m.maciorowski1991@gmail.com>
2023-01-18 21:09:21 +0100

    Cleaning up (commenting finished lessons code)

--- Lessons/ClassesAndInterfaces/interfacesAndClasses/app.js
+++ Lessons/ClassesAndInterfaces/interfacesAndClasses/app.js
@@ -1,16 +1,2 @@
 "use strict";
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    read(text) {
-        console.log(text);
-    }
-    greet(phrase) {
-        console.log(phrase, this.name, this.age);
-    }
-}
-const person = new Person('Marcin', 31);
-person.greet('Hello,');
 //# sourceMappingURL=app.js.map
--- Lessons/ClassesAndInterfaces/interfacesAndClasses/app.ts
+++ Lessons/ClassesAndInterfaces/interfacesAndClasses/app.ts
@@ -1,29 +1,29 @@
-interface Named {
-    readonly name: string
-    readonly lastName?: string
-}
-
-interface Greetable extends Named {
-    greet(phrase: string): void
-}
-
-interface Readable {
-    read(text: string): void
-}
-
-class Person implements Greetable, Readable {
-    constructor(public name: string, private age: number) {
-    }
-
-    read(text: string) {
-        console.log(text)
-    }
-
-    greet(phrase: string) {
-        console.log(phrase, this.name, this.age)
-    }
-}
-
-const person = new Person('Marcin', 31)
-
-person.greet('Hello,')
+// interface Named {
+//     readonly name: string
+//     readonly lastName?: string
+// }
+//
+// interface Greetable extends Named {
+//     greet(phrase: string): void
+// }
+//
+// interface Readable {
+//     read(text: string): void
+// }
+//
+// class Person implements Greetable, Readable {
+//     constructor(public name: string, private age: number) {
+//     }
+//
+//     read(text: string) {
+//         console.log(text)
+//     }
+//
+//     greet(phrase: string) {
+//         console.log(phrase, this.name, this.age)
+//     }
+// }
+//
+// const person = new Person('Marcin', 31)
+//
+// person.greet('Hello,')
