f2f238be4804b27597b08d0a5e431408bd2c779a
Marcin Maciorowski <m.maciorowski1991@gmail.com>
2023-01-18 21:25:27 +0100

    Working with intersection types

---
+++ Lessons/AdvancedTypes/IntersectionTypes/app.js
@@ -0,0 +1,12 @@
+"use strict";
+const e1 = {
+    name: 'Marcin',
+    privileges: ['pushing-code'],
+    startDate: new Date()
+};
+const e2 = {
+    name: 'Klaudia',
+    privileges: ['hiring-people'],
+    startDate: new Date()
+};
+//# sourceMappingURL=app.js.map
