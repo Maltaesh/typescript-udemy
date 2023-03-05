4b20c62c47846a16f2ebf25bd24d7903dd7da48b
Marcin Maciorowski <marmac2525@gmail.com>
2023-03-05 22:23:04 +0100

    Working with optional chaining

---
+++ Lessons/AdvancedTypes/OptionalChaining/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Marcin',
+    job: { title: "programmer", description: "fullstack" }
+};
+console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.description);
+//# sourceMappingURL=app.js.map
