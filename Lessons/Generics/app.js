84487d8765e3dd232a5c7b55078a321ff537197a
Marcin Maciorowski <marmac2525@gmail.com>
2023-03-12 17:05:12 +0100

    Basic info about generics

---
+++ Lessons/Generics/app.js
@@ -0,0 +1,19 @@
+"use strict";
+const names = ['Max', 'Manuel'];
+console.log(names);
+const promise = new Promise((res) => {
+    setTimeout(() => {
+        res('This is done');
+    }, 2000);
+});
+promise
+    .then(data => {
+    return data.split(' ');
+})
+    .then(data => {
+    console.log(data);
+})
+    .catch(err => {
+    console.error(err);
+});
+//# sourceMappingURL=app.js.map
---
+++ Lessons/Generics/app.ts
@@ -0,0 +1,22 @@
+const names: Array<string> = ['Max', 'Manuel']
+
+console.log(names)
+
+const promise: Promise<string> = new Promise((res) => {
+    setTimeout(() => {
+        res('This is done')
+    }, 2000)
+
+})
+
+promise
+    .then(data => {
+        return data.split(' ')
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.error(err)
+    })
+
