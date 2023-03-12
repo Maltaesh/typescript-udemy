543079fbcf2883aeadfae0f03445a9f7041dcf3d
Marcin Maciorowski <marmac2525@gmail.com>
2023-03-12 20:27:08 +0100

    Working with generic type constrains

--- Lessons/Generics/GenericFunctions/app.js
+++ Lessons/Generics/GenericFunctions/app.js
@@ -5,4 +5,18 @@ function merge(objA, objB) {
 console.log(merge({ name: "Marcin" }, { lastName: "Emski" }));
 console.log(merge({ 1: 1 }, { lastName: "Emski" }));
 console.log(merge({ '1': 1 }, { lastName: "Emski" }));
+function countAndDescribe(element) {
+    let description = 'Got no value.';
+    if (element.length === 1)
+        description = 'Got 1 element';
+    else if (element.length > 1)
+        description = 'Got ' + element.length + ' elements.';
+    return [element, description];
+}
+console.log(countAndDescribe('elelele'));
+console.log(countAndDescribe([1, 2]));
+function extractAndConvert(obj, key) {
+    return 'Value: ' + obj[key];
+}
+console.log(extractAndConvert({ name: "Marcin" }, 'name'));
 //# sourceMappingURL=app.js.map
